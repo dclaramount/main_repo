b6752090ce7f72ae5884a9fd8139ae06f9b69d9c
Diego Claramount <diegoaclaramount@gmail.com>
2025-02-06 18:14:52 +0100

    fix: adding other details of build.

--- modules/server/index.ts
+++ modules/server/index.ts
@@ -5,8 +5,17 @@ const port = 3000;
 
 app.get('/', (req, res)=>
 {
-     res.send('Hello World');
+     res.send(`Servicing App Build on ${process.env.BUILD_DATE}.`);
 });
+
+// Endpoint to serve environment variables
+app.get('/env', (req, res) => {
+     res.json({
+       BUILD_NUMBER: process.env.BUILD_NUMBER,
+       GIT_COMMIT: process.env.GIT_COMMIT,
+       BUILD_DATE: process.env.BUILD_DATE
+     });
+   });
 
  app.listen(port, ()=>{
      console.log(
